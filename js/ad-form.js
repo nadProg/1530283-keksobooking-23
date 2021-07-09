838d912db0bca7666cd4b78fa5ed128621b316cb
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-09 18:29:33 +0700

    Создает модуль синхронизации времени въезда/выезда

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,7 +1,8 @@
+import { commonNodes } from './utils.js';
 import { enableForm, disableForm } from './utils.js';
 import { initPrice, destroyPrice } from './price.js';
 import { initRoomCapacity, destroyRoomCapacity } from './room-capacity.js';
-import { commonNodes } from './utils.js';
+import { initCheckTime, destroyCheckTime} from './check-time.js';
 
 const { adFormNode }= commonNodes;
 
@@ -9,10 +10,13 @@ export const initAdForm = () => {
   enableForm(adFormNode);
   initPrice();
   initRoomCapacity();
+  initCheckTime();
 };
 
 export const destroyAdForm = () => {
+  adFormNode.reset();
   disableForm(adFormNode);
   destroyPrice();
   destroyRoomCapacity();
+  destroyCheckTime();
 };
---
+++ js/check-time.js
@@ -0,0 +1,30 @@
+import { commonNodes } from './utils.js';
+
+const { adFormNode } = commonNodes;
+
+const checkInNode = adFormNode.querySelector('#timein');
+const checkOutNode = adFormNode.querySelector('#timeout');
+
+const synchronizeTime = (currentTarget) => {
+  const bindTarget = currentTarget === checkInNode ? checkOutNode : checkInNode;
+  bindTarget.value = currentTarget.value;
+};
+
+const onCheckInNodeChange = ({ currentTarget }) => {
+  synchronizeTime(currentTarget);
+};
+
+const onCheckOutNodeChange = ({ currentTarget }) => {
+  synchronizeTime(currentTarget);
+};
+
+export const initCheckTime = () => {
+  checkInNode.addEventListener('change', onCheckInNodeChange);
+  checkOutNode.addEventListener('change', onCheckOutNodeChange);
+};
+
+export const destroyCheckTime = () => {
+  checkInNode.removeEventListener('change', onCheckInNodeChange);
+  checkOutNode.removeEventListener('change', onCheckOutNodeChange);
+};
+
