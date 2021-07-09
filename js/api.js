2023ff5f4560004ead2f771aefc0e45e4f6b1c61
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-09 09:28:16 +0700

    Создает модуль загрузки/отправки данных

---
+++ js/api.js
@@ -0,0 +1,30 @@
+import { isFunction } from './utils.js';
+
+const Url = {
+  POST: 'https://23.javascript.pages.academy/keksobooking',
+  GET: 'https://23.javascript.pages.academy/keksobooking/data',
+};
+
+const fetchData = async (method, onSuccess, onError, body) => {
+  method = method.toUpperCase();
+
+  try {
+    const response = await fetch(Url[method], { method, body });
+
+    if (!response.ok) {
+      throw new Error(`${response.status} — ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    return isFunction(onSuccess) ? onSuccess(result) : result;
+
+  } catch (error) {
+    if (isFunction(onError)) {
+      onError(error);
+    }
+  }
+};
+
+export const getData = (onSuccess, onError) => fetchData('GET', onSuccess, onError);
+
+export const postData = (onSuccess, onError, body) => fetchData('POST', onSuccess, onError, body);
