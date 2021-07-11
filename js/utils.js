debf1a0e21462435fffe8bfca399a1ced90a6623
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-11 11:05:11 +0700

    Удаляет неиспользуемые функции из utils

--- js/utils.js
+++ js/utils.js
@@ -1,43 +1,4 @@
-const HIDDEN_CLASS = 'hidden';
-const MODAL_OPEN_CLASS = 'modal-open';
-const TRAILING_SPACES = /[ ]+/g;
-const ALPHANUMERICALS = /^[а-яёa-z0-9]+$/;
-
 const bodyNode = document.body;
-
-/**
- * Проверяет соответствие длины строки максимально допустимому значению.
- * @param {string} string - Проверяемая строка.
- * @param {number} maxLength - Максимально допустимое значение длины строки.
- * @returns {boolean} Возвращает true, если длина строки не превышает максимально допустимого значения. Возвращает false, в остальных случаях.
- */
-export const checkMaxLength = (string, maxLength) => string.length <= maxLength;
-
-export const deleteTrailingSpaces = (string) => string.trim().replace(TRAILING_SPACES, ' ');
-
-export const isAlphaNumeric = (string) => ALPHANUMERICALS.test(string.toLowerCase());
-
-export const isArrayUnique = (items) => items.length === [...new Set(items)].length;
-
-/**
- * Перемешивает элементы массива случайным образом.
- * Использует алгоритм тасования Фишера-Йетса (источник: https://learn.javascript.ru/task/shuffle)
- * @param {Array} items - Исходный массив.
- * @returns {Array} Вовзращает перемешанную неглубокую копию исходного массива.
- */
-export const shuffle = (items) => {
-  items = [...items];
-
-  for (let i = items.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-
-    [items[i], items[j]] = [items[j], items[i]];
-  }
-
-  return items;
-};
-
-export const sortByComments = (pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length;
 
 export const debounce = (cb, time = 500) => {
   let timeoutId;
@@ -50,8 +11,6 @@ export const debounce = (cb, time = 500) => {
 
 export const isEscape = ({ code }) => code === 'Escape';
 
-export const isEnter = ({ code }) => code === 'Enter';
-
 export const isFunction = (func) => typeof func === 'function';
 
 export const renderNodes = (nodes, container) => {
@@ -59,14 +18,6 @@ export const renderNodes = (nodes, container) => {
   nodes.forEach((node) => fragment.appendChild(node));
   container.appendChild(fragment);
 };
-
-export const hideNode = (node) => node.classList.add(HIDDEN_CLASS);
-
-export const showNode = (node) => node.classList.remove(HIDDEN_CLASS);
-
-export const switchOnModalMode = () => bodyNode.classList.add(MODAL_OPEN_CLASS);
-
-export const switchOffModalMode = () => bodyNode.classList.remove(MODAL_OPEN_CLASS);
 
 const adFormNode = bodyNode.querySelector('.ad-form');
 
