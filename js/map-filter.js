aaff87b1f077dc82a6859d466ab6579f61f676d0
Eugene Skrobov <skrobov.ev@gmail.com>
2021-07-12 20:01:20 +0700

    Рефакторит map-filter

--- js/map-filter.js
+++ js/map-filter.js
@@ -1,7 +1,5 @@
 import { enableForm, disableForm, isFunction } from './utils.js';
 
-const ANY = 'any';
-
 const FilterPriceValue = {
   LOW: 'low',
   MIDDLE: 'middle',
@@ -16,8 +14,8 @@ const FilterPriceLimit = {
 const mapFilterNode = document.querySelector('.map__filters');
 const filterTypeNode = mapFilterNode.querySelector('#housing-type');
 const filterPriceNode = mapFilterNode.querySelector('#housing-price');
-const filterRoomsNode = mapFilterNode.querySelector('#housing-rooms');
-const filterGuestsNode = mapFilterNode.querySelector('#housing-guests');
+const filterRoomNode = mapFilterNode.querySelector('#housing-rooms');
+const filterGuestNode = mapFilterNode.querySelector('#housing-guests');
 const filterFeatureNodes = Array.from(mapFilterNode.querySelectorAll('.map__checkbox'));
 
 let initialOffers = null;
@@ -25,76 +23,70 @@ let filteredOffers = null;
 
 disableForm(mapFilterNode);
 
-const isAny = (value) => value === ANY;
+const isAny = (value) => value === 'any';
 
-const filterOffers = (offers) => {
-  const filterTypeValue = filterTypeNode.value;
-  const filterPriceValue = filterPriceNode.value;
-  const filterRoomsValue = filterRoomsNode.value;
-  const filterGuestsValue = filterGuestsNode.value;
-  const filterFeatureValues = Array.from(
+const getFilterValues = () => ({
+  type: filterTypeNode.value,
+  price: filterPriceNode.value,
+  room: filterRoomNode.value,
+  guest: filterGuestNode.value,
+  features: Array.from(
     filterFeatureNodes
       .filter(({ checked }) => checked)
       .map(({ value }) => value),
-  );
+  ),
+});
 
-  return offers.filter(({ offer}) => {
-    if (!isAny(filterTypeValue)) {
-      const isMatch = filterTypeValue === offer.type;
+const filterOffer = ({ offer }) => {
+  const {
+    type,
+    price,
+    room,
+    guest,
+    features,
+  } = getFilterValues();
 
-      if (!isMatch) {
-        return false;
-      }
+  if (!isAny(type) && !(type === offer.type)) {
+    return false;
+  }
+
+  if (!isAny(price)) {
+    let isMatch = true;
+    const offerPrice = Number(offer.price);
+
+    switch (price) {
+      case FilterPriceValue.LOW:
+        isMatch = offerPrice < FilterPriceLimit.LOW;
+        break;
+      case FilterPriceValue.MIDDLE:
+        isMatch = offerPrice >= FilterPriceLimit.LOW && offerPrice < FilterPriceLimit.HIGH;
+        break;
+      case FilterPriceValue.HIGH:
+        isMatch = offerPrice >= FilterPriceLimit.HIGH;
+        break;
     }
 
-    if (!isAny(filterPriceValue)) {
-      let isMatch = true;
-      const offerPrice = Number(offer.price);
-
-      switch (filterPriceValue) {
-        case FilterPriceValue.LOW:
-          isMatch = offerPrice < FilterPriceLimit.LOW;
-          break;
-        case FilterPriceValue.MIDDLE:
-          isMatch = offerPrice >= FilterPriceLimit.LOW && offerPrice < FilterPriceLimit.HIGH;
-          break;
-        case FilterPriceValue.HIGH:
-          isMatch = offerPrice >= FilterPriceLimit.HIGH;
-          break;
-      }
-
-      if (!isMatch) {
-        return false;
-      }
+    if (!isMatch) {
+      return false;
     }
+  }
 
-    if (!isAny(filterRoomsValue)) {
-      const isMatch = offer.rooms === Number(filterRoomsValue);
+  if (!isAny(room) && !(Number(room) === offer.rooms)) {
+    return false;
+  }
 
-      if (!isMatch) {
-        return false;
-      }
+  if (!isAny(guest) && !(Number(guest) === offer.guests)) {
+    return false;
+  }
+
+  if (features.length) {
+    const offerFeatures = new Set(offer.features);
+    if (!features.every((feature) => offerFeatures.has(feature))) {
+      return false;
     }
+  }
 
-    if (!isAny(filterGuestsValue)) {
-      const isMatch = offer.guests === Number(filterGuestsValue);
-
-      if (!isMatch) {
-        return false;
-      }
-    }
-
-    if (filterFeatureValues.length) {
-      const features = new Set(offer.features);
-      const isMatch = filterFeatureValues.every((value) => features.has(value));
-
-      if (!isMatch) {
-        return false;
-      }
-    }
-
-    return true;
-  });
+  return true;
 };
 
 const initialize = (offers, afterMapFilterNodeChange) => {
@@ -102,7 +94,7 @@ const initialize = (offers, afterMapFilterNodeChange) => {
   enableForm(mapFilterNode);
 
   mapFilterNode.addEventListener('change', () => {
-    filteredOffers = filterOffers(initialOffers);
+    filteredOffers = initialOffers.filter(filterOffer);
 
     if (isFunction(afterMapFilterNodeChange)) {
       afterMapFilterNodeChange();
